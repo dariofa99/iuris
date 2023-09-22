9e6a7cdd76fe9bcccf81424f53558bfd916551ba
dariogfa99 <darioj99@gmail.com>
2023-09-22 22:47:44 +0000

    circular2

--- resources/views/content/scripts.blade.php
+++ resources/views/content/scripts.blade.php
@@ -1,5 +1,5 @@
 <script>
-    @if (!Session::has('message-information'))
+    @if (Session::has('message-information'))
         var message = '';
         message += '<div class="alert alert-warning" style="font-size:18px">';
         message += `<h4>
@@ -7,7 +7,7 @@
         Bienvendido a {{ Str::upper(config('app.name')) }}!</strong> <br>
       Recuerda que estamos actualizando la plataforma, si presentas algún problema refresca el navegador
       con las teclas CTRL+F5 <i>o</i> CTRL+fn+F5 (portátiles). Tener en cuenta para conexión desde dispositivos móviles. <br>
-      <i> <small> Últ. Act 20 de sep. 2023 <br>
+      <i> <small> Últ. Act 22 de sep. 2023 <br>
         Si presentas algún problema comunícate al 3106038006  
       </small></i>
     </h4> </div`;
