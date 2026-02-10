cbaf3d5f3d2a1559e06f8977bc1e6833734e3151
dariofa99 <darioj99@gmail.com>
2026-02-10 17:44:36 -0500

    modal de exp

--- resources/views/myforms/frm_expediente_list.blade.php
+++ resources/views/myforms/frm_expediente_list.blade.php
@@ -45,13 +45,11 @@
                     <div class="row">
                         <div class="col-md-12">
                             @if (currentUser()->active_asignacion || currentUser()->hasRole('docente') || currentUser()->hasRole('docente_prueba'))
-                            <input type="hidden" name="search_onlyMy_exp" value="off">    
-                            <input type="checkbox" aria-describedby="desChkVerMisCasos"
-                                @if(!Request::has('search_onlyMy_exp') 
-                                || (Request::has('search_onlyMy_exp') and Request::input('search_onlyMy_exp') != 'off'))
-                                checked
-                                @endif
-                                 name="search_onlyMy_exp"
+                                <input type="hidden" name="search_onlyMy_exp" value="off">
+                                <input type="checkbox" aria-describedby="desChkVerMisCasos"
+                                    @if (
+                                        !Request::has('search_onlyMy_exp') ||
+                                            (Request::has('search_onlyMy_exp') and Request::input('search_onlyMy_exp') != 'off')) checked @endif name="search_onlyMy_exp"
                                     id="search_onlyMy_exp">
                                 <label for="search_onlyMy_exp">Solo listar mis casos asignados</label>
                                 {{--  <span id="desChkVerMisCasos" style="font-size: 0px !important;height: 0px;width:0px;position:absolute">
@@ -61,9 +59,7 @@
                         </div>
                         <div class="col-md-12">
                             <input type="checkbox" name="search_onlyProJur" value="search_onlyProJur" id="search_onlyProJur"
-                            @if(Request::has('search_onlyProJur'))
-                                checked
-                                @endif>
+                                @if (Request::has('search_onlyProJur')) checked @endif>
                             <label for="search_onlyProJur">
                                 Solo listar asuntos marcados como jurídicos
                             </label>
@@ -73,70 +69,71 @@
                     <div class="row">
                         <div class="col-md-3">
                             <label for="tipo_busqueda">Busqueda de expedientes</span>
-                            <select name="tipo_busqueda" id='tipo_busqueda' class="form-control form-control-sm"
-                                placeholder="Seleccione..." required="required">
-                                <option value="">Seleccione...</option>
-                                @if (currentUser()->hasRole('diradmin')
-                                 or currentUser()->hasRole('dirgral') 
-                                 or currentUser()->hasRole('amatai')  or currentUser()->hasRole('coordprac'))
+                                <select name="tipo_busqueda" id='tipo_busqueda' class="form-control form-control-sm"
+                                    placeholder="Seleccione..." required="required">
+                                    <option value="">Seleccione...</option>
+                                    @if (currentUser()->hasRole('diradmin') or
+                                            currentUser()->hasRole('dirgral') or
+                                            currentUser()->hasRole('amatai') or
+                                            currentUser()->hasRole('coordprac'))
+                                        <option
+                                            {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'idnumber_doc' ? 'selected' : '') }}
+                                            value="idnumber_doc">
+                                            Casos por docente
+                                        </option>
+                                    @endif
                                     <option
-                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'idnumber_doc' ? 'selected' : '') }}
-                                        value="idnumber_doc">
-                                        Casos por docente
-                                    </option>
-                                @endif
-                                <option
-                                    {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'codido_exp' ? 'selected' : '') }}
-                                    value="codido_exp">
-                                    Número de Expediente
-                                </option>
-
-                                @if (!currentUser()->hasRole('estudiante'))
-                                    <option value="estudiante_num"
-                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'estudiante_num' ? 'selected' : '') }}>
-                                        Documento de identificación (estudiante)
-                                    </option>
-                                    <option value="estudiante"
-                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'estudiante' ? 'selected' : '') }}>
-                                        Nombre o apellidos (estudiante)
+                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'codido_exp' ? 'selected' : '') }}
+                                        value="codido_exp">
+                                        Número de Expediente
                                     </option>
 
-                                    <option value="solicitante_num"
-                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'solicitante_num' ? 'selected' : '') }}>
-                                        Documento de identificación (solicitante)
+                                    @if (!currentUser()->hasRole('estudiante'))
+                                        <option value="estudiante_num"
+                                            {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'estudiante_num' ? 'selected' : '') }}>
+                                            Documento de identificación (estudiante)
+                                        </option>
+                                        <option value="estudiante"
+                                            {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'estudiante' ? 'selected' : '') }}>
+                                            Nombre o apellidos (estudiante)
+                                        </option>
+
+                                        <option value="solicitante_num"
+                                            {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'solicitante_num' ? 'selected' : '') }}>
+                                            Documento de identificación (solicitante)
+                                        </option>
+                                    @endif
+                                    <option
+                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'solicitante' ? 'selected' : '') }}
+                                        value="solicitante">
+                                        Nombre o apellidos (solicitante)</option>
+                                    <option
+                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'estado' ? 'selected' : '') }}
+                                        value="estado">
+                                        Estado
                                     </option>
-                                @endif
-                                <option
-                                    {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'solicitante' ? 'selected' : '') }}
-                                    value="solicitante">
-                                    Nombre o apellidos (solicitante)</option>
-                                <option
-                                    {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'estado' ? 'selected' : '') }}
-                                    value="estado">
-                                    Estado
-                                </option>
-                                <option
-                                    {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'tipo_consulta' ? 'selected' : '') }}
-                                    value="tipo_consulta">Tipo de Consulta</option>
-                                <option
-                                    {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'rama_derecho' ? 'selected' : '') }}
-                                    value="rama_derecho">Rama del Derecho</option>
-                                <option
-                                    {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'fecha_creacion' ? 'selected' : '') }}
-                                    value="fecha_creacion">
-                                    Fecha de Creación
-                                </option>
-                              {{--   <option
+                                    <option
+                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'tipo_consulta' ? 'selected' : '') }}
+                                        value="tipo_consulta">Tipo de Consulta</option>
+                                    <option
+                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'rama_derecho' ? 'selected' : '') }}
+                                        value="rama_derecho">Rama del Derecho</option>
+                                    <option
+                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'fecha_creacion' ? 'selected' : '') }}
+                                        value="fecha_creacion">
+                                        Fecha de Creación
+                                    </option>
+                                    {{--   <option
                                     {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'fecha_rango' ? 'selected' : '') }}
                                     value="fecha_rango">
                                     Rango Fechas
                                 </option> --}}
-                                <option
-                                    {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'all' ? 'selected' : '') }}
-                                    value="all">
-                                    Todo (limpiar)
-                                </option>
-                            </select>
+                                    <option
+                                        {{ Request::has('tipo_busqueda') and (Request::get('tipo_busqueda') == 'all' ? 'selected' : '') }}
+                                        value="all">
+                                        Todo (limpiar)
+                                    </option>
+                                </select>
                         </div>
                         <div class="col-md-5">
                             <span>Ingrese un valor</span>
@@ -150,8 +147,8 @@
                                 'data-live-search-placeholder' => 'Escriba un valor...',
                             ]) !!}
 
-                            <input type="date" required id="data_date" name="data" class="form-control form-control-sm"
-                                style="display: none">
+                            <input type="date" required id="data_date" name="data"
+                                class="form-control form-control-sm" style="display: none">
 
 
                             <table width="100%" style="display: none;width:100% !important">
@@ -189,14 +186,14 @@
             </div>
 
             <div class="col-md-2" id="content_count_asesorias_inlist">
-                 {{--  @include('myforms.components_exp.count_asesorias_inlist') --}}
+                {{--  @include('myforms.components_exp.count_asesorias_inlist') --}}
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
                 <div id="table_list_model">
 
-                     @include('myforms.frm_expediente_list_ajax') 
+                    @include('myforms.frm_expediente_list_ajax')
 
                 </div>
             </div>
@@ -206,17 +203,50 @@
 
 
     @include('myforms.frm_modal_buscar_exp_avanzada')
+
+
+    <!-- ================= MODAL IMAGEN PRO ================= -->
+    <div class="modal fade modal-image-pro" id="imagePreviewModal" tabindex="-1">
+
+        <div class="modal-dialog modal-dialog-centered modal-xl">
+
+            <div class="modal-content bg-transparent border-0 shadow-none">
+
+                <button class="close btn-close-pro" data-dismiss="modal">
+                    &times;
+                </button>
+
+                <div class="text-center">
+
+                    <img id="previewImage" class="img-fluid preview-img">
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
 @stop
 @push('scripts')
     <!-- aqui van los scripts de cada vista -->
     <!-- Latest compiled and minified JavaScript -->
     <script src="{{ asset('/plugins/bootstrap-select/bootstrap.js') }}"></script>
 
-    <script type="module" src={{ asset('js/admin_expedientes.js?v='. config('app_config.asset_version')) }}></script>
+    <script type="module" src={{ asset('js/admin_expedientes.js?v=' . config('app_config.asset_version')) }}></script>
     <script>
-        $(document).ready(function () {
-            var page = "expedientes";     
-           // let res = index_page(page, {});
+        $(document).ready(function() {
+            var page = "expedientes";
+            // let res = index_page(page, {});
+        });
+    </script>
+    <script>
+        $(document).on("click", ".avatar-click", function() {
+            const src = $(this).data("img");
+            $("#previewImage").attr("src", src);
+            $("#imagePreviewModal").modal("show");
         });
     </script>
 @endpush
--- resources/views/myforms/frm_modal_detalles_expedientes.blade.php
+++ resources/views/myforms/frm_modal_detalles_expedientes.blade.php
@@ -5,150 +5,153 @@
 
 
     @slot('title')
-       Detalles del caso
+        Detalles del caso
     @endslot
 
 
     @slot('body')
-<div class="container-fluid expediente-pro py-3">
+        <div class="container-fluid expediente-pro py-3">
 
-    <div class="row">
+            <div class="row">
 
-        <!-- ===================================================== -->
-        <!-- PERFIL ESTUDIANTE -->
-        <!-- ===================================================== -->
-        <div class="col-lg-4 mb-4">
+                <!-- ===================================================== -->
+                <!-- PERFIL ESTUDIANTE -->
+                <!-- ===================================================== -->
+                <div class="col-lg-4 mb-4">
 
-            <div class="pro-card text-center h-100">
+                    <div class="pro-card text-center h-100">
 
-                <img class="avatar mb-3"
-                    src="{{ is_file(public_path('thumbnails/' . $expediente->estudiante->image))
-                        ? asset('thumbnails/' . $expediente->estudiante->image)
-                        : asset('thumbnails/default.jpg') }}">
+                        <img class="avatar mb-3 avatar-click"
+                            src="{{ is_file(public_path('thumbnails/' . $expediente->estudiante->image))
+                                ? asset('thumbnails/' . $expediente->estudiante->image)
+                                : asset('thumbnails/default.jpg') }}"
+                            data-img="{{ is_file(public_path('thumbnails/' . $expediente->estudiante->image))
+                                ? asset('thumbnails/' . $expediente->estudiante->image)
+                                : asset('thumbnails/default.jpg') }}">
 
-                <h5 class="font-weight-bold mb-1">
-                    {{ FullName($expediente->estudiante->name, $expediente->estudiante->lastname) }}
-                </h5>
 
-                <small class="text-muted d-block mb-3">
-                    {{ $expediente->estudiante->idnumber }}
-                </small>
+                        <h5 class="font-weight-bold mb-1">
+                            {{ FullName($expediente->estudiante->name, $expediente->estudiante->lastname) }}
+                        </h5>
 
-                <div class="chips justify-content-center mb-3">
-                    <span class="chip chip-primary">
-                        {{ $expediente->estudiante->curso->ref_nombre }}
-                    </span>
+                        <small class="text-muted d-block mb-3">
+                            {{ $expediente->estudiante->idnumber }}
+                        </small>
+
+                        <div class="chips justify-content-center mb-3">
+                            <span class="chip chip-primary">
+                                {{ $expediente->estudiante->curso->ref_nombre }}
+                            </span>
+                        </div>
+
+                        <hr>
+
+                        <div class="text-left small">
+                            <p><i class="fas fa-phone mr-2"></i> {{ $expediente->estudiante->tel1 }}</p>
+                            <p><i class="fas fa-map-marker-alt mr-2"></i> {{ $expediente->estudiante->address }}</p>
+                        </div>
+
+                    </div>
                 </div>
 
-                <hr>
 
-                <div class="text-left small">
-                    <p><i class="fas fa-phone mr-2"></i> {{ $expediente->estudiante->tel1 }}</p>
-                    <p><i class="fas fa-map-marker-alt mr-2"></i> {{ $expediente->estudiante->address }}</p>
+                <!-- ===================================================== -->
+                <!-- INFORMACIÓN -->
+                <!-- ===================================================== -->
+                <div class="col-lg-8">
+
+                    <!-- =================== EXPEDIENTE =================== -->
+                    <div class="pro-card mb-4">
+
+                        <h6 class="section-title">
+                            <i class="fas fa-folder-open mr-2"></i>
+                            Información del Expediente
+                        </h6>
+
+                        <div class="info-grid">
+
+                            <div class="info-item">
+                                <span>Docente</span>
+                                <b>{{ $expediente->getDocenteAsig()->name }} {{ $expediente->getDocenteAsig()->lastname }}</b>
+                            </div>
+
+                            <div class="info-item">
+                                <span>Código</span>
+                                <b>{{ $expediente->expid }}</b>
+                            </div>
+
+                            <div class="info-item">
+                                <span>Rama del derecho</span>
+                                <b>{{ $expediente->rama_derecho->ramadernombre }}</b>
+                            </div>
+
+                            <div class="info-item">
+                                <span>Fecha creación</span>
+                                <b>{{ getSmallDateWithHour($expediente->created_at) }}</b>
+                            </div>
+
+                            <div class="info-item">
+                                <span>Fecha de asignación</span>
+                                @if ($expediente->asignacion)
+                                    <b>{{ getSmallDateWithHour($expediente->asignacion->fecha_asig) }}</b>
+                                @else
+                                    <b>—</b>
+                                @endif
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <!-- =================== SOLICITANTE =================== -->
+                    @if (!currentUser()->hasRole('solicitante'))
+                        <div class="pro-card">
+
+                            <h6 class="section-title">
+                                <i class="fas fa-user mr-2"></i>
+                                Datos del Solicitante
+                            </h6>
+
+                            <div class="info-grid">
+
+                                <div class="info-item">
+                                    <span>Identificación</span>
+                                    <b>{{ $expediente->solicitante->idnumber ?? '—' }}</b>
+                                </div>
+
+                                <div class="info-item">
+                                    <span>Nombre</span>
+                                    <b>
+                                        {{ $expediente->solicitante
+                                            ? FullName($expediente->solicitante->name, $expediente->solicitante->lastname)
+                                            : 'Sin solicitante' }}
+                                    </b>
+                                </div>
+
+                                <div class="info-item">
+                                    <span>Teléfono</span>
+                                    <b>
+                                        {{ $expediente->solicitante->tel1 ?? '—' }}
+                                        @if (isset($expediente->solicitante->tel2) && $expediente->solicitante->tel2 != '')
+                                            - {{ $expediente->solicitante->tel2 }}
+                                        @endif
+                                    </b>
+                                </div>
+
+                                <div class="info-item">
+                                    <span>Dirección</span>
+                                    <b>{{ $expediente->solicitante->address ?? '—' }}</b>
+                                </div>
+
+                            </div>
+                        </div>
+                    @endif
+
                 </div>
 
             </div>
         </div>
-
-
-        <!-- ===================================================== -->
-        <!-- INFORMACIÓN -->
-        <!-- ===================================================== -->
-        <div class="col-lg-8">
-
-            <!-- =================== EXPEDIENTE =================== -->
-            <div class="pro-card mb-4">
-
-                <h6 class="section-title">
-                    <i class="fas fa-folder-open mr-2"></i>
-                    Información del Expediente
-                </h6>
-
-                <div class="info-grid">
-
-                    <div class="info-item">
-                        <span>Docente</span>
-                        <b>{{ $expediente->getDocenteAsig()->name }} {{ $expediente->getDocenteAsig()->lastname }}</b>
-                    </div>
-
-                    <div class="info-item">
-                        <span>Código</span>
-                        <b>{{ $expediente->expid }}</b>
-                    </div>
-
-                    <div class="info-item">
-                        <span>Rama del derecho</span>
-                        <b>{{ $expediente->rama_derecho->ramadernombre }}</b>
-                    </div>
-
-                    <div class="info-item">
-                        <span>Fecha creación</span>
-                        <b>{{  getSmallDateWithHour($expediente->created_at) }}</b>
-                    </div>
-
-                    <div class="info-item">
-                        <span>Fecha de asignación</span>
-                        @if($expediente->asignacion)
-                            <b>{{ getSmallDateWithHour($expediente->asignacion->fecha_asig) }}</b>
-                        @else
-                            <b>—</b>
-                        @endif
-                        
-                    </div>
-
-                </div>
-            </div>
-
-
-            <!-- =================== SOLICITANTE =================== -->
-            @if (!currentUser()->hasRole('solicitante'))
-            <div class="pro-card">
-
-                <h6 class="section-title">
-                    <i class="fas fa-user mr-2"></i>
-                    Datos del Solicitante
-                </h6>
-
-                <div class="info-grid">
-
-                    <div class="info-item">
-                        <span>Identificación</span>
-                        <b>{{ $expediente->solicitante->idnumber ?? '—' }}</b>
-                    </div>
-
-                    <div class="info-item">
-                        <span>Nombre</span>
-                        <b>
-                            {{ $expediente->solicitante
-                                ? FullName($expediente->solicitante->name,$expediente->solicitante->lastname)
-                                : 'Sin solicitante' }}
-                        </b>
-                    </div>
-
-                    <div class="info-item">
-                        <span>Teléfono</span>
-                        <b>
-                            {{ $expediente->solicitante->tel1 ?? '—' }}
-                            @if(isset($expediente->solicitante->tel2) && $expediente->solicitante->tel2 != '')
-                                - {{ $expediente->solicitante->tel2 }}
-                            @endif
-                        </b>
-                    </div>
-
-                    <div class="info-item">
-                        <span>Dirección</span>
-                        <b>{{ $expediente->solicitante->address ?? '—' }}</b>
-                    </div>
-
-                </div>
-            </div>
-            @endif
-
-        </div>
-
-    </div>
-</div>
-
     @endslot
 @endcomponent
 <!-- /modal -->
